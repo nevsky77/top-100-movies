d68f2675813d734dc29060b93d69812659fd3442
user <user@MacBook-Pro-user.local>
2020-03-07 22:50:14 +0200

    hm| d3-force

---
+++ src/components/D3ForceComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>D3-Force</div>
+</template>
+
+<script>
+  export default {
+    name: "D3ForceComponent"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/D3Chart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <d3-force-component></d3-force-component>
+  </div>
+</template>
+
+<script>
+  import D3ForceComponent from "../components/D3ForceComponent";
+  export default {
+    name: "D3Chart",
+    components: {D3ForceComponent}
+  }
+</script>
+
+<style scoped>
+
+</style>
