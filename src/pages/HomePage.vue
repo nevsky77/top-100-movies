2d1629466fd9ff6d5345c4cd294d1b92bc6fc300
user <user@MacBook-Pro-user.local>
2020-03-23 13:08:51 +0200

    fix style pagination

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,12 +8,12 @@
           <option v-for="o in decades" :value="o.value">{{o.name}}</option>
         </select>
       </div>
-      <button
-        type="button"
-        class="btn btn-outline-secondary"
-        variant="primary"
-        @click="reverseMoviesList">Sort: <span class="caret" :class="{'caret-down': caretReverse}"></span>
-      </button>
+<!--      <button-->
+<!--        type="button"-->
+<!--        class="btn btn-outline-secondary"-->
+<!--        variant="primary"-->
+<!--        @click="reverseMoviesList">Sort: <span class="caret" :class="{'caret-down': caretReverse}"></span>-->
+<!--      </button>-->
     </div>
 
     <hr class="py-2">
@@ -33,12 +33,14 @@
           v-model="page"
           :page-count="pageCount"
           :click-handler="paginateChangeHandler"
-          :prev-text="'&laquo;'"
+          :next-class="'page-item'"
+          :next-link-class="'page-link'"
+          :prev-class="'page-item'"
+          :prev-link-class="'page-link'"
           :next-text="'&raquo;'"
+          :prev-text="'&laquo;'"
           :container-class="'pagination'"
           :page-link-class="'page-link'"
-          :prev-class="'page-link'"
-          :next-class="'page-link'"
           :page-class="'page-item'">
         </Paginate>
       </div>
@@ -187,7 +189,14 @@
     cursor: pointer;
     font-size: 50px;
   }
-  
+  .page-link {
+    color:#42b983;
+  }
+  .page-item.active .page-link {
+    background-color: #42b983;
+    border-color: #42b983;
+  }
+
   @media (max-width: 600px) {
     .home {
       grid-template-columns: 1fr 1fr;
